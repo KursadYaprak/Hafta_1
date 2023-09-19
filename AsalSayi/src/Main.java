ad975b2bb421ef1913089c608f8e7f9e2929e25a
Kürşad Yaprak <kursadyaprak80@hotmail.com>
2023-09-19 08:32:08 +0300

    Girilen Sayıya Kadar Olan Asal Sayıları Yazdıran Program

--- AsalSayi/src/Main.java
+++ AsalSayi/src/Main.java
@@ -5,28 +5,21 @@ public class Main {
 
         Scanner inp = new Scanner(System.in);
 
-        int number, check = 0;
+        int check = 0;
 
-        System.out.print("Bir sayı giriniz : ");
-        number = inp.nextInt();
-
-        if (number < 2) {
-            System.out.println("En küçük asal sayı 2'dir. Başka bir sayı giriniz.");
-        } else {
-            System.out.print("2 ");
-            for (int i = 2; i <= number; i++) {
-                for (int k = 2; k < i; k++) {
-                    if (i % k != 0) {
-                        check++;
-                        if (check == i - 2) {
-                            System.out.print(i + " ");
-                            check = 0;
-                            break;
-                        }
-                    } else {
+        System.out.print("2 ");
+        for (int i = 2; i <= 100; i++) {
+            for (int k = 2; k < i; k++) {
+                if (i % k != 0) {
+                    check++;
+                    if (check == i - 2) {
+                        System.out.print(i + " ");
                         check = 0;
                         break;
                     }
+                } else {
+                    check = 0;
+                    break;
                 }
             }
         }
