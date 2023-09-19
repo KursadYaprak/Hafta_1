ff02e8f38ef460599e16fdc65e3ed338725b6d42
Kürşad Yaprak <kursadyaprak80@hotmail.com>
2023-09-19 06:50:01 +0300

    Girilen Sayının Mükemmel Sayı Olup Olmadığını Söyleyen Program

---
+++ MukemmelSayi/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        int n, toplam = 0;
+
+        System.out.print("Bir sayı giriniz : ");
+        n = inp.nextInt();
+
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                toplam += i;
+            }
+        }
+        if (toplam == n) {
+            System.out.println(n + " Bir Mükemmel sayıdır.");
+        } else {
+            System.out.println(n + " Bir Mükemmel sayı değildir.");
+        }
+    }
+}
