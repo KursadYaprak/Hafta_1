417f4b37cd8413a49c2d1e59f68a33dd1f907169
Kürşad Yaprak <kursadyaprak80@hotmail.com>
2023-09-19 09:13:45 +0300

    1 Ile 100 Arasındaki Asal Sayıları Ekrana Yazdıran Program

--- AsalSayi/src/Main.java
+++ AsalSayi/src/Main.java
@@ -1,26 +1,18 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        Scanner inp = new Scanner(System.in);
-
-        int check = 0;
-
-        System.out.print("2 ");
         for (int i = 2; i <= 100; i++) {
+
+            boolean asal = true;
+
             for (int k = 2; k < i; k++) {
-                if (i % k != 0) {
-                    check++;
-                    if (check == i - 2) {
-                        System.out.print(i + " ");
-                        check = 0;
-                        break;
-                    }
-                } else {
-                    check = 0;
+                if (i % k == 0) {
+                    asal = false;
                     break;
                 }
+            }
+            if (asal) {
+                System.out.print(i + " ");
             }
         }
         System.out.println();
